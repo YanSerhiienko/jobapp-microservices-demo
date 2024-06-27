ca8f411c7198768d17856b00f631bef4c419cf84
Yan Serhiienko <yanserhiienko@gmail.com>
2024-06-27 19:08:22 +0300

    Resilience4J

--- jobms/src/main/java/com/example/jobms/job/impl/JobServiceImpl.java
+++ jobms/src/main/java/com/example/jobms/job/impl/JobServiceImpl.java
@@ -9,6 +9,9 @@ import com.example.jobms.job.clients.ReviewClient;
 import com.example.jobms.job.dto.JobDTO;
 import com.example.jobms.job.dto.JobMapper;
 import com.example.jobms.job.external.Review;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
@@ -17,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -30,11 +34,20 @@ public class JobServiceImpl implements JobService {
     private final CompanyClient companyClient;
     private final ReviewClient reviewClient;
 
+    //@CircuitBreaker(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+    //@Retry(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+    @RateLimiter(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
     @Override
     public List<JobDTO> findAllJobs() {
         List<Job> jobs = jobRepository.findAll();
 
         return jobs.stream().map(this::convertToDTO).collect(Collectors.toList());
+    }
+
+    public List<String> companyBreaker(Exception e) {
+        List<String> list = new ArrayList<>();
+        list.add("Dummy");
+        return list;
     }
 
     private JobDTO convertToDTO(Job job) {
